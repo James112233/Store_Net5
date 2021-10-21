560bcdbb52ab92ec1593e55f7894ba8557716ace
James112233 <jamesliu847@hotmail.com>
2021-10-21 19:02:53 +1100

    Added Router, Product Details page

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -7,6 +7,11 @@ import {
 } from "@mui/material";
 import Header from "./Hearder";
 import { useState } from "react";
+import { Route } from "react-router";
+import HomePage from "../../features/home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -29,7 +34,11 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} themeChangeHandler={themeChangeHandler} />
       <Container>
-        <Catalog />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/catalog" component={Catalog} />
+        <Route path="/catalog/:id" component={ProductDetails} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
       </Container>
     </ThemeProvider>
   );
